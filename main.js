5769446be33ee11366072563e919198e97835e45
Maria <maria.heinrich@student.uibk.ac.at>
2022-06-02 17:09:53 +0200

    Wettervorhersage overlays mit Popup bei Innsbruck

--- main.js
+++ main.js
@@ -77,7 +77,12 @@ loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europ
 async function loadWeather(url) {
     const response = await fetch(url);
     const jsondata = await response.json();
-    console.log("geoJsonData:", jsondata);
-    
+    // console.log("geoJsonData:", jsondata);
+
+    layerControl.addOverlay(overlays.weather, "Wettervorhersage met. no");
+
+    let marker = L.circleMarker([
+        47.267222, 11.392778 // s. URL loadWeather
+    ]).bindPopup("Wettervorhersage").addTo(overlays.weather);
 };
 loadWeather("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
